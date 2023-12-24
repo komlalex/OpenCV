8c7b43bf3bcb8d19441e7825e7b854f8934709d8
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-24 13:17:11 +0000

    Image pyramids

---
+++ image_pyramid.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread("my_image.jpeg")
+
+smaller = cv2.pyrDown(img)
+larger = cv2.pyrUp(img)
+
+cv2.imshow("Smaller", smaller)
+cv2.imshow("Original", img)
+cv2.imshow("Larger", larger)
+
+cv2.waitKey(0)
